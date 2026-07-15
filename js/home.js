0e9abd5b517180e23fbfa69d0a597143443f1b87
tayaibrafsun28 <tayaibrafsun28@gmail.com>
2026-07-15 21:34:18 +0600

    added toggle function

--- js/home.js
+++ js/home.js
@@ -23,14 +23,22 @@ calculateCards()
 
 
 function toggleStyle(id) {
+
     const allToggleBtn = document.getElementById('all-toggle-btn');
+
     const interviewToggleBtn = document.getElementById('interview-toggle-btn');
+
     const rejectedTogglebtn = document.getElementById('rejected-toggle-btn');
 
-    allToggleBtn.classList.remove('btn-primary');
-    interviewToggleBtn.classList.remove('btn-primary');
-    rejectedTogglebtn.classList.remove('btn-primary');
 
+    allToggleBtn.classList.add('btn-secondary');
+    interviewToggleBtn.classList.add('btn-secondary');
+    rejectedTogglebtn.classList.add('btn-secondary');
     
+
+    const selectedBtn = document.getElementById(id)
+
+    selectedBtn.classList.remove('btn-secondary')
+    selectedBtn.classList.add('btn-primary')
 
 }
